4537fd1cc7d13d8885d8f66599b261018fa28301
Marcio Jose Martins Camara <118389170+marciomartins12@users.noreply.github.com>
2023-05-26 13:17:39 -0300

    Add files via upload

--- script/validacoes.js
+++ script/validacoes.js
@@ -14,20 +14,23 @@ function enviaNumeroFalado(numerofalado){
         <a class="btn-voltar-para-o-inicio" href="../index.html">Voltar para o inicio</a>
         </section>`;
     }else if(numerox<numerosecreto){
+        document.querySelector(".fale-iniciar-obs").classList.add("displaynone");
         document.querySelector(".mais-ou-menos").innerHTML=`<p class="numerosrecetoe" >O número secreto é maior</p>
         <span>
         <i class="fa-solid fa-arrow-up fa-bounce"></i>
             </span>`
     }
     else if(numerox>numerosecreto){
+        document.querySelector(".fale-iniciar-obs").classList.add("displaynone");
         document.querySelector(".mais-ou-menos").innerHTML=`<p>O número secreto é menor</p>
         <span>
         <i class="fa-solid fa-arrow-down fa-bounce"></i>
             </span>`
     }
     else if(numerox<0 || numerox>1000){
+        document.querySelector(".fale-iniciar-obs").classList.add("displaynone");
         document.querySelector(".mais-ou-menos").innerHTML=`<p>Apenas numeros enre 0 e 1000</p>`
     }
 }
 
-console.log("e numero sereco:", numerosecreto)
+console.log("número Secreto:", numerosecreto);
